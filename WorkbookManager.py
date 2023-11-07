bfc84e7ba007e146dff519f4e461042d7584b79e
alexandarmatev <alexandarmatev@gmail.com>
2023-11-07 08:54:52 +0200

    Implemented caching and adding two new methods for getting all scope controls and getting control by id.

--- WorkbookManager.py
+++ WorkbookManager.py
@@ -8,6 +8,9 @@ class WorkbookManager:
     def __init__(self, workbook_path):
         self._workbook_path = workbook_path
         self._workbook = openpyxl.load_workbook(self._workbook_path)
+        self._cache = None
+        self._scope_levels = WorkbookManager.get_scope_levels()
+        self._populate_cache()
 
     @property
     def path(self):
@@ -17,6 +20,8 @@ class WorkbookManager:
     def path(self, new_path):
         self._workbook_path = new_path
         self._workbook = openpyxl.load_workbook(self._workbook_path)
+        self._cache = None
+        self._populate_cache()
 
     @classmethod
     def get_scope_levels(cls):
@@ -29,30 +34,46 @@ class WorkbookManager:
 
         return worksheet, header_row, column_indices
 
-    def get_all_scope_controls(self, scope_level=None):
-        scope_levels = WorkbookManager.get_scope_levels()
+    def _populate_cache(self):
+        self._cache = {'MacOS L1': {'Sonoma 14.0': []}, 'MacOS L2': {'Sonoma 14.0': []}}
+        for level in self._scope_levels:
+            worksheet, header_row, column_indices = self._get_worksheet_attributes(level)
+            for row in worksheet.iter_rows(min_row=2, values_only=True):
+                control_id = row[column_indices['Recommendation #']]
+                title = row[column_indices['Title']]
+                description = row[column_indices['Description']]
+                header = False
 
-        if (scope_level and scope_level not in scope_levels) or not scope_level:
+                if not control_id:
+                    control_id = row[column_indices['Section #']]
+                    header = True
+
+                control = Control(control_id, title, description, level, header)
+                self._cache[f'MacOS L{level}']['Sonoma 14.0'].append({control_id: control})
+
+    def get_scope_controls(self, *, scope_level=None):
+        if (scope_level and scope_level not in self._scope_levels) or not scope_level:
             scope_level = 1
+        return self._cache[f'MacOS L{scope_level}']['Sonoma 14.0']
 
-        worksheet, header_row, column_indices = self._get_worksheet_attributes(scope_level)
+    def get_all_scope_controls(self):
+        return self._cache
 
-        all_scope_controls = []
-        for row in worksheet.iter_rows(min_row=2, values_only=True):
-            control_id = row[column_indices['Recommendation #']]
-            title = row[column_indices['Title']]
-            description = row[column_indices['Description']]
-            level = scope_level
-            header = False
+    def get_control_by_id(self, *, control_level=None, control_id=None):
+        if control_level not in self._scope_levels:
+            raise ValueError(f'{control_level} is not in the scope levels.')
+        if not control_id:
+            raise ValueError('Control ID must be provided.')
 
-            if not control_id:
-                control_id = row[column_indices['Section #']]
-                header = True
+        control_id = str(control_id)
+        scope_controls = self.get_scope_controls(scope_level=control_level)
+        control_by_id = (control.get(control_id) for control in scope_controls if control.get(control_id))
 
-            control = Control(control_id, title, description, level, header)
-            all_scope_controls.append(control)
+        try:
+            return next(control_by_id)
+        except StopIteration:
+            raise KeyError(f'{control_id} is not in the level {control_level} controls.')
 
-        return all_scope_controls
 
 
 
--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from WorkbookManager import WorkbookManager
 workbook = WorkbookManager('cis_benchmarks/CIS_Apple_macOS_14.0_Sonoma_Benchmark_v1.0.0.xlsx')
 all_controls = workbook.get_all_scope_controls()
 
-for control in all_controls:
-    print(control)
+print(workbook.get_scope_controls(scope_level=1))
+print(workbook.get_control_by_id(control_level=2, control_id=1))
