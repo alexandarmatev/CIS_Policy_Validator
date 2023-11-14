2f28079c37ca56056566dd68680da2f1376599cc
alexandarmatev <alexandarmatev@gmail.com>
2023-11-14 09:00:21 +0200

    1. Made the cache initialization and the benchmark profiles OS independent. 2. Created scope level → os mapping method and replaced the initialization mechanism of cache and headers. 3. Created new validator method for validating and returning the benchmark profile for a specific scope level.

--- WorkbookManager.py
+++ WorkbookManager.py
@@ -1,22 +1,24 @@
-import re
-
 from DataModels import Recommendation, RecommendHeader
 import openpyxl
 import os
+import re
 from openpyxl.worksheet.worksheet import Worksheet
 from typing import Dict, Tuple, Set, List, Iterator
 
 
 class WorkbookManager:
     _SCOPE_LEVELS = {1, 2}
+    _BENCHMARK_PROFILES_REX = r'(Level\s(\d)\s-\s\w+ \d+\.\d{1,2}(?:\s\w+)*)'
 
     def __init__(self, workbook_path: str):
         self._workbook_path = self._validate_and_return_file_path(workbook_path)
         self._workbook = openpyxl.load_workbook(self._workbook_path)
+        self._scope_levels = self.get_scope_levels()
+        self._benchmark_profile_rex = self.get_benchmark_profiles_rex()
+        self._scope_levels_os_mapping = self._populate_scope_levels_os_mapping()
+        self._benchmark_profiles = self._get_benchmark_profiles()
         self._cache = None
         self._headers = None
-        self._scope_levels = WorkbookManager.get_scope_levels()
-        self._benchmark_profiles = self._get_benchmark_profiles()
         self._populate_cache_and_headers()
 
     @staticmethod
@@ -47,15 +49,40 @@ class WorkbookManager:
     def get_scope_levels(cls) -> Set:
         return cls._SCOPE_LEVELS
 
+    @classmethod
+    def get_benchmark_profiles_rex(cls) -> str:
+        return cls._BENCHMARK_PROFILES_REX
+
+    @property
+    def benchmark_profiles(self) -> List[Tuple]:
+        return self._get_benchmark_profiles()
+
+    @property
+    def scope_levels_os_mapping(self):
+        return self._scope_levels_os_mapping
+
     def _get_benchmark_profiles(self) -> list:
-        regex_pattern = r'(Level\s\d\s-\s\w+ \d+\.\d{1,2}(?:\s\w+)*)'
+        regex_pattern = self.get_benchmark_profiles_rex()
         overview_worksheet = self._workbook['Overview - Glossary']
         overview_paragraphs = str([paragraph for paragraph in overview_worksheet.iter_rows(values_only=True)])
         return re.findall(regex_pattern, overview_paragraphs)
 
-    @property
-    def benchmark_profiles(self) -> list:
-        return self._benchmark_profiles
+    def _populate_scope_levels_os_mapping(self) -> Dict:
+        benchmark_profiles = self._get_benchmark_profiles()
+        allowed_levels = self.get_scope_levels()
+        scope_levels_os_mapping = {level: [] for level in allowed_levels}
+        for profile, profile_level in benchmark_profiles:
+            profile_level = int(profile_level)
+            for level in allowed_levels:
+                if profile_level == level:
+                    scope_levels_os_mapping[profile_level].append(profile)
+        return scope_levels_os_mapping
+
+    def _validate_and_return_benchmark_scope_profile(self, scope_level: int) -> str:
+        scope_level_os = self._scope_levels_os_mapping[scope_level]
+        if not scope_level_os:
+            raise ValueError(f'Benchmark profile for level {scope_level} does not exist.')
+        return next(iter(scope_level_os))
 
     def _validate_and_return_scope_level(self, scope_level: int) -> int:
         if not isinstance(scope_level, int):
@@ -72,7 +99,7 @@ class WorkbookManager:
 
     def _validate_and_get_items_by_type(self, scope_level: int, item_type: str) -> List[Dict[str, Recommendation]] | List[Dict[str, RecommendHeader]]:
         if item_type.casefold() == 'recommendation':
-            scope_items = self.get_scope_recommendations(scope_level=scope_level)
+            scope_items = self.get_recommendations_scope(scope_level=scope_level)
         elif item_type.casefold() == 'recommend_header':
             scope_items = self.get_recommendations_scope_headers(scope_level=scope_level)
         else:
@@ -101,25 +128,37 @@ class WorkbookManager:
 
             yield recommend_id, title, description, assessment_method, is_header
 
-    def _populate_cache_and_headers(self):
-        self._cache = {'MacOS Sonoma L1': [], 'MacOS Sonoma L2': []}
-        self._headers = {'level 1': [], 'level 2': []}
+    def _initialize_cache_headers_keys(self):
+        cache_headers_mapping = {}
+        for level, benchmark_profiles in self._scope_levels_os_mapping.items():
+            for profile in benchmark_profiles:
+                cache_headers_mapping[profile] = []
+        return cache_headers_mapping
 
-        for level in self._scope_levels:
+    def _populate_cache_and_headers(self):
+        cache_headers_mapping = self._initialize_cache_headers_keys()
+        self._cache = cache_headers_mapping
+        self._headers = cache_headers_mapping
+
+        for level, benchmark_profiles in self._scope_levels_os_mapping.items():
             worksheet, column_indices = self._get_worksheet_scope_headers(level)
             worksheet_row_attrs = self._get_worksheet_recommendation_attributes(worksheet, column_indices)
 
-            for recommend_id, title, description, assessment_method, is_header in worksheet_row_attrs:
-                if is_header:
-                    header = RecommendHeader(header_id=recommend_id, title=title, description=description, level=level)
-                    self._headers[f'level {level}'].append({recommend_id: header})
-                    continue
-                recommendation = Recommendation(recommend_id=recommend_id,
-                                                title=title,
-                                                description=description,
-                                                assessment_method=assessment_method,
-                                                level=level)
-                self._cache[f'MacOS Sonoma L{level}'].append({recommend_id: recommendation})
+            for profile in benchmark_profiles:
+                for recommend_id, title, description, assessment_method, is_header in worksheet_row_attrs:
+                    if is_header:
+                        header = RecommendHeader(header_id=recommend_id,
+                                                 title=title,
+                                                 description=description,
+                                                 level=level)
+                        self._headers[profile].append({recommend_id: header})
+                        continue
+                    recommendation = Recommendation(recommend_id=recommend_id,
+                                                    title=title,
+                                                    description=description,
+                                                    assessment_method=assessment_method,
+                                                    level=level)
+                    self._cache[profile].append({recommend_id: recommendation})
 
     def get_item_by_id(self, *, scope_level: int = 1, item_id: str, item_type: str) -> Recommendation | RecommendHeader:
         scope_level = self._validate_and_return_scope_level(scope_level)
@@ -132,18 +171,18 @@ class WorkbookManager:
 
         raise KeyError(f'{item_type.capitalize()} with ID {item_id} is not in level {scope_level} of {item_type}s.')
 
-    def get_scope_recommendations(self, *, scope_level: int = 1) -> List[Dict[str, Recommendation]]:
+    def get_recommendations_scope(self, *, scope_level: int = 1) -> List[Dict[str, Recommendation]]:
         scope_level = self._validate_and_return_scope_level(scope_level)
-        return self._cache[f'MacOS Sonoma L{scope_level}']
+        scope_profile = self._validate_and_return_benchmark_scope_profile(scope_level)
+        return self._cache[scope_profile]
 
-    def get_all_recommendations(self) -> Dict[str, List[Dict[str, Recommendation]]]:
+    def get_all_scopes_recommendations(self) -> Dict[str, List[Dict[str, Recommendation]]]:
         return self._cache
 
     def get_recommendations_scope_headers(self, *, scope_level: int = 1) -> List[Dict[str, RecommendHeader]]:
         scope_level = self._validate_and_return_scope_level(scope_level)
-        return self._headers[f'level {scope_level}']
+        scope_profile = self._validate_and_return_benchmark_scope_profile(scope_level)
+        return self._headers[scope_profile]
 
-    def get_all_recommendation_headers(self) -> Dict[str, List[Dict[str, RecommendHeader]]]:
+    def get_all_scopes_recommendation_headers(self) -> Dict[str, List[Dict[str, RecommendHeader]]]:
         return self._headers
-
-
--- main.py
+++ main.py
@@ -10,39 +10,42 @@ path = 'cis_benchmarks/CIS_Apple_macOS_14.0_Sonoma_Benchmark_v1.0.0.xlsx'
 # Creating a class instance of WorkbookManager
 workbook = WorkbookManager(path)
 
-print(workbook.benchmark_profiles)
-#
-# # Test getter method of path
-# print(workbook.path)
-#
-# # Test get_scope_levels class method
-# print(workbook.get_scope_levels())
-#
-# # Test get_item_by_id method without providing a scope level (default scope level is 1)
-# print(workbook.get_item_by_id(item_id='1.1', item_type='recommendation'))
-#
-# # Test get_item_by_id method for getting recommendation and recommendation header for the different control scopes
-# print(workbook.get_item_by_id(scope_level=1, item_id='1.1', item_type='recommendation'))
-# print(workbook.get_item_by_id(scope_level=2, item_id='2.1.1.1', item_type='recommendation'))
-#
-# print(workbook.get_item_by_id(scope_level=1, item_id='1', item_type='recommend_header'))
-# print(workbook.get_item_by_id(scope_level=2, item_id='2', item_type='recommend_header'))
-#
-# # Test get_scope_recommendations method without providing a scope level (default scope level is 1)
-# print(workbook.get_scope_recommendations())
-#
-# # Test get_scope_recommendations for scope level 2
-# print(workbook.get_scope_recommendations(scope_level=2))
-#
-# # Test get_all_recommendations method
-# print(workbook.get_all_recommendations())
-#
-# # Test get_recommendations_scope_headers method without providing a scope level (default scope level is 1)
-# print(workbook.get_recommendations_scope_headers())
-#
-# # Test get_recommendations_scope_headers for scope level 2
-# print(workbook.get_recommendations_scope_headers(scope_level=2))
-#
-# # Test get_all_recommendation_headers method
-# print(workbook.get_all_recommendation_headers())
+print(workbook._initialize_cache_headers_keys())
+print(workbook.scope_levels_os_mapping)
+# print(workbook.get_all_scopes_recommendations())
+
+
+# Test getter method of path
+print(workbook.path)
+
+# Test get_scope_levels class method
+print(workbook.get_scope_levels())
+
+# Test get_item_by_id method without providing a scope level (default scope level is 1)
+print(workbook.get_item_by_id(item_id='1.1', item_type='recommendation'))
+
+# Test get_item_by_id method for getting recommendation and recommendation header for the different control scopes
+print(workbook.get_item_by_id(scope_level=1, item_id='1.1', item_type='recommendation'))
+print(workbook.get_item_by_id(scope_level=2, item_id='2.1.1.1', item_type='recommendation'))
+
+print(workbook.get_item_by_id(scope_level=1, item_id='1', item_type='recommend_header'))
+print(workbook.get_item_by_id(scope_level=2, item_id='2', item_type='recommend_header'))
+
+# Test get_scope_recommendations method without providing a scope level (default scope level is 1)
+print(workbook.get_recommendations_scope())
+
+# Test get_scope_recommendations for scope level 2
+print(workbook.get_recommendations_scope(scope_level=2))
+
+# Test get_all_recommendations method
+print(workbook.get_all_scopes_recommendations())
+
+# Test get_recommendations_scope_headers method without providing a scope level (default scope level is 1)
+print(workbook.get_recommendations_scope_headers())
+
+# Test get_recommendations_scope_headers for scope level 2
+print(workbook.get_recommendations_scope_headers(scope_level=2))
+
+# Test get_all_recommendation_headers method
+print(workbook.get_all_scopes_recommendation_headers())
 
